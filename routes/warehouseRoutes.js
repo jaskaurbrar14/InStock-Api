7058aff96609f4f01b77bd1f875172537ab31481
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-08 23:04:39 -0500

    warehouse.js file modified

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -2,12 +2,18 @@ const knex = require("knex")(require("../knexfile"));
 const router = express.Router();
 //const { v4: uuidv4 } = require("uuid");
 
-router.get("/", async (_req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-    const data = await knex("user");
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(400).send(`Error retrieving Users: ${err}`);
+    const { id } = req.params;
+    const warehouseFound = await knex("warehouses").where({ id }).first();
+
+    if (!warehouseFound) {
+      return res.status(404).json({ error: "Warehouse not found" });
+    }
+
+    res.status(200).json(warehouseFound);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
