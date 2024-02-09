4091a24afab5ece0b308894a5344aac819d558ed
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-08 22:22:19 -0500

    dotenv config added to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+require("dotenv").config();
 const PORT = process.env.PORT || 8081;
 
 const CLIENT_URL = process.env.CLIENT_URL;
