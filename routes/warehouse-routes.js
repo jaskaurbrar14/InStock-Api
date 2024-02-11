772e34e0f6de295df49a7b9954aaaf53f9d15e0c
Elyse <elyse.waslat@gmail.com>
2024-02-11 16:34:45 -0500

    hook Tejal's getWarehouseById to /warehouses/:warehouse_id

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const warehouseController = require("../controllers/warehouse-controller");
 
 router.get("/warehouses", warehouseController.index);
+router.get("/warehouses/:warehouse_id", warehouseController.getWarehouseById);
 router.put("/warehouses/:warehouse_id", warehouseController.edit);
 router.delete("/warehouses/:id", warehouseController.remove);
 
