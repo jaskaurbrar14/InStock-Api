e026901cafc1344cce124320d8758740bd9add9e
Bita Fotovvat <bita.ftvt@gmail.com>
2024-02-10 22:36:03 -0500

    added delete route to inventory-routes

--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -4,4 +4,6 @@ const inventoryController = require('../controllers/inventory-controller');
 
 router.get('/warehouses/:warehouse_id/inventories/:inventory_id', inventoryController.findById);
 
+router.delete('/inventories/:id', inventoryController.remove);
+
 module.exports = router;
