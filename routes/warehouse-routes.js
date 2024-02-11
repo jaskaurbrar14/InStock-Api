b7adc193a04f1b1f6e4fed05ea43336b5c31e8a7
TejalAGunjal <tejal.a.gunjal@gmail.com>
2024-02-10 21:19:38 -0500

    controller.js file modified

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
 const warehouseController = require("../controllers/warehouse-controller");
-router.use(express.json());
+
 router.get("/warehouses", warehouseController.index);
 
 //Get single Warehouse
 
-router.get("/:id", warehouseController.getWarehouseById);
+router.get("/:id", warehouseController.getSingleWarehouse);
 
 module.exports = router;
