c1d556fa211671146d23386d341d1f71f8a31f57
Bita Fotovvat <bita.ftvt@gmail.com>
2024-02-11 13:06:35 -0500

    delete functionality added to the backend

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -72,8 +72,27 @@ function validatePhoneNumber(phoneNumber) {
         return false;
     }
 }
-
+const remove = async (req, res) => {
+    console.log(`Attempting to delete warehouse with ID ${req.params.id}`);
+    try {
+      const rowsDeleted = await knex('warehouses')
+        .where({ id: req.params.id })
+        .delete();
+  
+      if (rowsDeleted === 0) {
+        return res
+          .status(404)
+          .json({ message: `Warehouse with ID ${req.params.id} not found` });
+      }
+      res.sendStatus(204);
+    } catch (error) {
+      res.status(500).json({
+        message: `Unable to delete warehouse: ${error}`
+      });
+    }
+  };
 module.exports = {
     index,
-    edit, 
+    edit,
+    remove
 };
--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -4,5 +4,6 @@ const warehouseController = require('../controllers/warehouse-controller');
 
 router.get('/warehouses', warehouseController.index);
 router.put('/warehouses/:warehouse_id', warehouseController.edit);
+router.delete('/warehouses/:id', warehouseController.remove);
 
 module.exports = router;
