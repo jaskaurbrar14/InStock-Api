4db80e872faf0bd09dae810519be9fcbf062e3ee
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-02-12 07:07:40 +0530

    re add the code for create item and edit item

--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
-const router = require("express").Router();
 
-const inventoryController = require("../controllers/inventory-controller");
 const inventoryController = require("../controllers/inventory-controller");
 
 router.get(
